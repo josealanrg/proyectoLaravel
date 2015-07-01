f7298ed5aedf3eb2aa02b1b525e3c5e33b47e86e
Edder jose <edder_salo@hotmail.com>
2015-06-30 20:49:07 -0600

    Modelos y ruta /grupos V2 , Index modificado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,24 +21,3 @@
 @stop
 
 
-@section ('grupos')
-
-
-     <article id="articulo2">
-
-  <div class="panel panel-primary" id="panelgrupos">
-      <div class="panel-heading">
-        <h3 class="panel-title">Grupos disponibles</h3>
-      </div>
-      <div class="panel-body" >
-
-      
-      </div>
-    </div>
-
-    </article>
-    
-    
-
-@stop
-
